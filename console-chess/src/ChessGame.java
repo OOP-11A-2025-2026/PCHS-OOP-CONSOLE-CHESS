1baf7034e61e0ee0e936f76fb311e8c4f28a1d94
YordanTsonev <yordantsonev20@gmail.com>
2025-12-16 19:29:07 +0200

    Add timer for 10 minutes

--- console-chess/src/ChessGame.java
+++ console-chess/src/ChessGame.java
@@ -1,20 +1,21 @@
 import board.Board;
 import board.Square;
-import cli.PieceRenderer;
 import cli.BoardPrinter;
-import pieces.Piece;
-import enums.PieceType;
 import enums.Color;
 import pieces.Rook;
+import timer.GameTimer;
 
 public class ChessGame {
+
     public static void main(String[] args) {
         Board board = new Board();
-
         board.setPieceAt(new Square(0, 0), new Rook(Color.WHITE, 0, 0));
         board.setPieceAt(new Square(0, 7), new Rook(Color.BLACK, 0, 7));
-
+        GameTimer timer = new GameTimer(10);
+        timer.start();
         BoardPrinter.print(board);
+
+        System.out.println("⏱ Game started. White to move.");
+        timer.printTime();
     }
 }
-
